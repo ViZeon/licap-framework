b51c6ed550a1d3b2da5b2faa013d161ade8ce931
zieviox <zieviox@gmail.com>
2025-10-11 07:14:13 -0400

    LiCapLight is now it's own Node

--- Scripts/Systems/Lighting/Nodes/LiCapLight.gd
+++ Scripts/Systems/Lighting/Nodes/LiCapLight.gd
@@ -1,11 +1,26 @@
+@tool
 extends Node3D
 class_name LiCapLight
 
-# Called when the node enters the scene tree for the first time.
+@export var light_control: Node: 
+	get:
+		return light_control
+	set(detected_conrol):
+		light_control = detected_conrol
+@export var light_control_initialized = false
+
+@export var light_id: int = -1
+
 func _ready() -> void:
-	pass # Replace with function body.
+	find_light_control()
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if not light_control_initialized:
+		find_light_control()
+		notify_property_list_changed()
+		light_control_initialized = true
+		light_id = light_control.register_light(self)
+
+func find_light_control():
+	light_control = get_owner().find_child("SceneLightControl")
+	print (light_control)
